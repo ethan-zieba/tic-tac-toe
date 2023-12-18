85161c0e0332c6d0520bafc9be19b5fb05fb8e46
Ethan <146005489+ethan-zieba@users.noreply.github.com>
2023-12-18 18:35:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,92 +1,85 @@
 import pygame as pg
+import customtkinter as ctk
+import ia
 
 
-board = []
-
-
-class Case:
-    def __init__(self, state):
-        self.state = state
-        self.state_str = ""
-        match self.state:
-            case 0:
-                self.state_str = "Blank"
-            case 1:
-                self.state_str = "Cross"
-            case 2:
-                self.state_str = "Circle"
-            case _:
-                self.state_str = "Blank"
-
-    def __str__(self):
-        return f"State: {self.state_str}"
-
-    def __repr__(self):
-        return f"State: {self.state_str}"
-
-
-def create_board():
-    global board
-    for i in range(3):
-        board.append([])
-        for u in range(3):
-            board[i].append(Case(0))
-
-
-def place_case(coord, type):
-    board[coord[1]][coord[0]] = Case(type)
-
-
-who = 1
+board = [[0 for u in range(3)] for i in range(3)]
+who = 0
 
 
 def turn(coord):
     x, y = coord[0] // 300, coord[1] // 300
     global who
-    if board[y][x].state != 0:
+    if board[y][x] != 0:
         who = 0 if who == 1 else 1
         return False
-    place_case((x, y), who+1)
+    board[y][x] = who + 1
+    if not check_win():
+        if ia_active:
+            print(ia.get_best_move(board))
+            case_tuple = ia.get_best_move(board)
+            board[case_tuple[1]][case_tuple[0]] = who + 2 if who == 0 else 1
+            who = 0 if who == 1 else 1
+        who = 0 if who == 1 else 1
+    else:
+        return False
 
 
-def win_interface(winner = 2):
+def win_interface(winner = 3):
+    if winner == False:
+        return False
     global is_over
     is_over = True
-    back_rect = pg.draw.rect(screen, color=(255, 255, 255), rect=(100, 300, 700, 300))
-    match (winner):
-        case 0:
-            text = rob_font.render("Cross wins", True, (0, 0, 0))
-            screen.blit(text, (127, 360))
-        case 1:
-            text = rob_font.render("Circle wins", True, (0, 0, 0))
-            screen.blit(text, (127, 360))
-        case 2:
-            text = rob_font.render("Draw", True, (0, 0, 0))
-            screen.blit(text, (200, 360))
+    back_rect = pg.draw.rect(screen, color=(200, 200, 200), rect=(100, 300, 700, 300))
+    if winner == 1:
+        text = rob_font.render("Cross wins", True, (0, 0, 0))
+        screen.blit(text, (127, 360))
+    elif winner == 2:
+        text = rob_font.render("Circle wins", True, (0, 0, 0))
+        screen.blit(text, (127, 360))
+    elif winner == 3:
+        text = rob_font.render("Draw", True, (0, 0, 0))
+        screen.blit(text, (200, 360))
 
 
 def check_win():
     global board
-    is_equality = False
-    for e in board:
-        for el in e:
-            if el.state == 0:
-                is_equality = True
     for i in range(3):
-        if 0 != board[i][0].state == board[i][1].state == board[i][2].state:
-            win_interface(who)
+        if 0 != board[i][0] == board[i][1] == board[i][2]:
+            return board[i][0]
+        if 0 != board[0][i] == board[1][i] == board[2][i]:
+            return board[0][i]
+    if 0 != board[0][0] == board[1][1] == board[2][2] or 0 != board[0][2] == board[1][1] == board[2][0]:
+        return board[i][0]
+    return check_draw()
+
+
+def check_draw():
     for i in range(3):
-        if 0 != board[0][i].state == board[1][i].state == board[2][i].state:
-            win_interface(who)
-    if 0 != board[0][0].state == board[1][1].state == board[2][2].state or 0 != board[0][2].state == board[1][1].state == board[2][0].state:
-        win_interface(who)
-    elif not is_equality:
-        win_interface()
+        for u in range(3):
+            if board[i][u] == 0:
+                return False
+    return 3
+
+def open_menu():
+    root = ctk.CTk()
+    root.geometry("230x200")
+    root.title("Start Menu")
+    ia_active = ctk.BooleanVar()
+    frame = ctk.CTkFrame(root)
+    titleLabel = ctk.CTkLabel(root, width=200, font=("Roboto", 40), height=100, text="Tic Tac Toe")
+    startButton = ctk.CTkButton(root, width=100, height=50, text="Start", command=root.destroy, font=('Roboto', 20))
+    checkIA = ctk.CTkCheckBox(root, height=10, variable =ia_active, onvalue = True,
+                              offvalue = False, text="Play against IA ?")
+    checkIA.grid(row=2, column=1, padx=10, pady=10)
+    startButton.grid(row=3, column=1, padx=10)
+    titleLabel.grid(row=1, column=1, padx=10)
+    root.mainloop()
+    return ia_active.get()
 
 
-
-create_board()
-
+ia_active = open_menu()
+ia_color = 1
 pg.init()
 dimensions = (900, 900)
 screen = pg.display.set_mode(dimensions)
@@ -100,7 +93,7 @@ while running:
         if event.type == pg.QUIT:
             running = False
         if event.type == pg.MOUSEBUTTONUP and not is_over:
-            who = 0 if who == 1 else 1
+            print(ia_active)
             pos = pg.mouse.get_pos()
             turn(pos)
             print("\n")
@@ -109,10 +102,13 @@ while running:
     for i in range(3):
         for u in range(3):
             pg.draw.rect(screen, color=(255, 255, 255), rect=(i*300, u*300, 300, 300), width=3)
-            if board[i][u].state == 1:
-                pg.draw.line(screen, color=(255, 255, 255), start_pos=(u*300, i*300), end_pos=(u*300+300, i*300+300), width=3)
-                pg.draw.line(screen, color=(255, 255, 255), start_pos=(u*300, i*300+300), end_pos=(u*300+300, i*300), width=3)
-            elif board[i][u].state == 2:
+            if board[i][u] == 1:
+                pg.draw.line(screen, color=(255, 255, 255), start_pos=(u*300, i*300),
+                             end_pos=(u*300+300, i*300+300), width=3)
+                pg.draw.line(screen, color=(255, 255, 255), start_pos=(u*300, i*300+300),
+                             end_pos=(u*300+300, i*300), width=3)
+            elif board[i][u] == 2:
                 pg.draw.circle(screen, color=(255, 255, 255), center=(u*300+150, i*300+150), radius=140)
-    check_win()
+    ended = check_win()
+    win_interface(ended)
     pg.display.flip()
